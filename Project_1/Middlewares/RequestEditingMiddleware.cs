8c427e59394e422648e3af6a2a0725c199313250
Ramazan  TOY <ramcotoy06@gmail.com>
2024-04-28 20:36:44 +0300

    request editing middleware created

---
+++ Project_1/Middlewares/RequestEditingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Project_1.Middlewares
+{
+    public class RequestEditingMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+
+        public RequestEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+        
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.ToString().Equals("/ramo"))
+            {
+                await context.Response.WriteAsync("welcome ramo");
+            }
+            else
+            {
+                await _requestDelegate.Invoke(context);
+            }
+      
+        }
+    }
+}
