2fe5a88908201102f46007fafa50c1d69ef265c3
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-04 00:32:28 +0300

    FolderController created, list.html created

---
+++ Project_1/Controllers/FolderController.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_1.Controllers
+{
+    public class FolderController : Controller
+    {
+        public IActionResult List()
+        {
+            var directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"));
+            var directories =directoryInfo.GetDirectories();
+            return View(directories);
+        }
+    }
+}
