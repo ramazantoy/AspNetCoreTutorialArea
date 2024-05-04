a04cba5bf5a0626a8f70c1858d830897c5696d49
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-04 15:46:31 +0300

    file create and file remove created

--- Project_1/Controllers/FolderController.cs
+++ Project_1/Controllers/FolderController.cs
@@ -11,5 +11,31 @@ namespace Project_1.Controllers
             var directories =directoryInfo.GetDirectories();
             return View(directories);
         }
+        [HttpPost]
+        public IActionResult Create(string folderName)
+        {
+            var info=new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot",folderName));
+            if (!info.Exists)
+            {
+                info.Create();
+            }
+            return RedirectToAction("List");
+        }
+        
+        public IActionResult Remove(string folderName)
+        {
+            var info=new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot",folderName));
+            if (info.Exists)
+            {
+                info.Delete();
+            }
+            return RedirectToAction("List");
+        }
+
+     
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
