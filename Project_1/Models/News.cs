9c0a06befb090f9dfc9fdc08b98e3d045aa91f8a
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-05 16:19:13 +0300

    partial view added to home page

---
+++ Project_1/Models/News.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_1.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ Project_1/Models/NewsContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Project_1.Models
+{
+    public static class NewsContext
+    {
+        public static List<News> News = new()
+        {
+            new News{Title = "News_1"},
+            new News { Title = "News_2"}
+        };
+    }
+}
