efff39dc8099808884054a39221bbfbb0a8f1f9e
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-05 16:49:14 +0300

    custom tag helper added home page

---
+++ Project_1/TagHelpers/ParagraphTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Project_1.TagHelpers
+{
+    [HtmlTargetElement("paragraph")]
+    public class ParagraphTagHelper : TagHelper
+    {
+        
+        public string ShortDescription { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"<b>Custom tag helper works {ShortDescription}</b>");
+            base.Process(context, output);
+        }
+    }
+}
