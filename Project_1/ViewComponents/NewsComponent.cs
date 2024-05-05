775d566b32be59d387a242fbc6d213fa39da9d3f
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-05 16:30:52 +0300

    view component added home page

---
+++ Project_1/ViewComponents/NewsComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_1.Models;
+
+namespace Project_1.ViewComponents
+{
+    public class NewsComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var news = NewsContext.News;
+            return View(news);
+        }
+    }
+}
