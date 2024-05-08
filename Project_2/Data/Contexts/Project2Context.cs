f6b2c238368f62fc8fb9423f0ace80ed3e4a8946
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-08 20:34:16 +0300

    fluent api annotations added

--- Project_2/Data/Contexts/Project2Context.cs
+++ Project_2/Data/Contexts/Project2Context.cs
@@ -14,10 +14,18 @@ namespace Project_2.Data.Contexts
                 "server=(localdb)\\mssqllocaldb; database=Project2Core; integrated security=true;");
         }
 
-        // protected override void OnModelCreating(ModelBuilder modelBuilder) //fluent api ovverride to dataannotations
-        // {
-        //     modelBuilder.Entity<Category>().ToTable(name: "Categories", schema: "dbo");
-        //     base.OnModelCreating(modelBuilder);
-        // }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //fluent api ovverride to dataannotations
+        {
+            modelBuilder.Entity<Category>().ToTable(name: "category");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("category_name").HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("category_id").IsRequired();
+
+            modelBuilder.Entity<Product>().ToTable(name: "product");
+            modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("product_name").HasMaxLength(50).HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("product_id").IsRequired();
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasColumnName("product_price").HasMaxLength(50)
+                .IsRequired();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
