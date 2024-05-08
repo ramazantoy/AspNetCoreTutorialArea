d5cfa57ff8ef41698684151672f35497d6be1cbd
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-08 20:16:52 +0300

    testing on data annotation column settings snake_case

--- Project_2/Data/Entities/Category.cs
+++ Project_2/Data/Entities/Category.cs
@@ -1,11 +1,18 @@
-﻿// using System.ComponentModel.DataAnnotations.Schema;
+﻿// using System.ComponentModel.DataAnnotations;
+// using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project_2.Data.Entities
 {
     // [Table(name:"Category",Schema = "c")] // for table name and shema write add-migrate ... and update-database
     public class Category
     {
+        // [Column("category_id")] 
         public int Id { get; set; }
+        
+        // [Required]
+        // [MaxLength(50)]
+        //for category_name  and max length
+        // [Column("category_name",TypeName = "nvarchar(50)")]
         public string Name { get; set; }
     }
 }
