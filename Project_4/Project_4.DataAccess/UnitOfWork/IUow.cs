18c2b7cf42c2b6090a31361ef29d202732633f1e
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-14 14:50:58 +0300

    DataAccess : Unit of work added

---
+++ Project_4/Project_4.DataAccess/UnitOfWork/IUow.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Project_4.DataAccess.Interfaces;
+
+namespace Project_4.DataAccess.UnitOfWork
+{
+    public interface IUow
+    {
+        IRepository<T> GetRepository<T>() where T : class, new();
+
+        Task SaveChanges();
+    }
+}
---
+++ Project_4/Project_4.DataAccess/UnitOfWork/Uow.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Project_4.DataAccess.Contexts;
+using Project_4.DataAccess.Interfaces;
+using Project_4.DataAccess.Repositories;
+
+namespace Project_4.DataAccess.UnitOfWork
+{
+    public class Uow : IUow
+    {
+
+        private readonly TodoContext _todoContext;
+
+        public Uow(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class, new()
+        {
+
+            return new Repository<T>(_todoContext);
+        }
+
+        public  async Task SaveChanges()
+        {
+            await _todoContext.SaveChangesAsync();
+        }
+    }
+}
