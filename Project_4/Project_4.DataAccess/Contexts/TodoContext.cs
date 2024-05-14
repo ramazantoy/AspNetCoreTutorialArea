d628b0f5c048b964ee159788f2b04011091bdfe4
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-14 13:03:11 +0300

    packages added for data acces, works and context created

---
+++ Project_4/Project_4.DataAccess/Contexts/TodoContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_4.DataAccess.Contexts
+{
+    public class TodoContext
+    {
+        
+    }
+}
---
+++ Project_4/Project_4.Entities/Domains/Work.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_4.Entities.Domains
+{
+    public class Work
+    {
+        public int Id { get; set; }
+
+        public string Definition { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
