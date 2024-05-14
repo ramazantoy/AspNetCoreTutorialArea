ae7ba32986fd8c89eeb87dd6bdd55816c4f153ba
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-14 13:15:21 +0300

    work configuration with fluent api

---
+++ Project_4/Project_4.DataAccess/Configurations/WorkConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_4.Entities.Domains;
+
+namespace Project_4.DataAccess.Configurations
+{
+    public class WorkConfiguration : IEntityTypeConfiguration<Work>
+    {
+        public void Configure(EntityTypeBuilder<Work> builder)
+        {
+            
+            builder.ToTable("works");
+            
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("work_id");
+            
+            
+            builder.Property(x => x.Definition).HasColumnName("definition").HasMaxLength(50).IsRequired();
+
+            builder.Property(x => x.IsCompleted).HasColumnName("is_completed").IsRequired();
+            
+        }
+    }
+}
--- Project_4/Project_4.DataAccess/Contexts/TodoContext.cs
+++ Project_4/Project_4.DataAccess/Contexts/TodoContext.cs
@@ -1,7 +1,22 @@
-﻿namespace Project_4.DataAccess.Contexts
+﻿using Microsoft.EntityFrameworkCore;
+using Project_4.DataAccess.Configurations;
+using Project_4.Entities.Domains;
+
+namespace Project_4.DataAccess.Contexts
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
-        
+        public DbSet<Work> Works { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new WorkConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
