5f1f8a30b0acfea3149f0475a3409614e9c6d48b
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-14 14:45:50 +0300

    DataAcces : IRepositoy<T> and Repository<T> created

---
+++ Project_4/Project_4.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Project_4.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T: class,new()
+    {
+        Task<List<T>> GetAll();
+
+        Task <T> GetById(int id);
+
+        Task<T> GetByFilter(Expression<Func<T, bool>> filter, bool asNoTracking = false);
+
+        Task Create(T entity);
+
+        void Remove(T entity);
+        
+        void Update(T entity);
+
+    }
+}
---
+++ Project_4/Project_4.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project_4.DataAccess.Contexts;
+using Project_4.DataAccess.Interfaces;
+
+namespace Project_4.DataAccess.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly TodoContext _todocontext;
+
+        public Repository(TodoContext todocontext)
+        {
+            _todocontext = todocontext;
+        }
+
+        public async Task Create(T entity)
+        {
+            await _todocontext.Set<T>().AddAsync(entity);
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _todocontext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+    
+        public async Task<T> GetByFilter(Expression<Func<T, bool>> filter, bool asNoTracking = false)
+        {
+            return asNoTracking ? await _todocontext.Set<T>().SingleOrDefaultAsync(filter) : await _todocontext.Set<T>().AsNoTracking().SingleOrDefaultAsync(filter);
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _todocontext.Set<T>().FindAsync(id);
+        }
+
+
+        public void Remove(T entity)
+        {
+            _todocontext.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _todocontext.Set<T>().Update(entity);
+        }
+    }
+}
