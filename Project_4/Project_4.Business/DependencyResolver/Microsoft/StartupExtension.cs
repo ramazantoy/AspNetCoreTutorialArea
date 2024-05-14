58a94bc00e72026437ad8031cb7201c96f3d7482
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-14 13:51:29 +0300

    StartUpExtension created, data migration created and pushed db

---
+++ Project_4/Project_4.Business/DependencyResolver/Microsoft/StartupExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Project_4.DataAccess.Contexts;
+
+namespace Project_4.Business.DependencyResolver.Microsoft
+{
+    public  static class StartupExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>
+            {
+                opt.UseSqlServer("server=(localdb)\\mssqllocaldb; database=Project4Core; integrated security=true;");
+            });
+
+        }
+    }
+}
