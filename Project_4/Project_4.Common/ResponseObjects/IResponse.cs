56e10a6a5d4cbd866eaa26f1b801ed0c98ce3d62
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-24 18:05:42 +0300

    Project_4.Common : Response and IResponse created

---
+++ Project_4/Project_4.Common/ResponseObjects/IResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_4.Common.ResponseObjects
+{
+    public interface IResponse
+    {
+        string Message { get; set; }
+
+        ResponseType ResponseType { get; set; }
+    }
+}
---
+++ Project_4/Project_4.Common/ResponseObjects/Response.cs
@@ -0,0 +1,28 @@
+﻿namespace Project_4.Common.ResponseObjects
+{
+    public class Response : IResponse
+    {
+        public Response(ResponseType responseType)
+        {
+            ResponseType = responseType;
+        }
+        
+        public Response(ResponseType responseType,string message)
+        {
+            ResponseType = responseType;
+            Message = message;
+        }
+        
+        
+        public string Message { get; set; }
+        
+        public ResponseType ResponseType { get; set; }
+    }
+
+    public enum ResponseType
+    {
+        Success,
+        ValidationError,
+        NotFound,
+    }
+}
