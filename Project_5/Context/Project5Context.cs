143d6bf47fc61bbfb66fd55c72cda445e69859fd
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-25 17:59:49 +0300

    inital database created, first migration created, connected to mssql db

---
+++ Project_5/Context/Project5Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_5.Context
+{
+    public class Project5Context : IdentityDbContext
+    {
+        public Project5Context(DbContextOptions<Project5Context> options) : base(options)
+        {
+            
+        }
+    }
+}
--- Project_5/Startup.cs
+++ Project_5/Startup.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project_5.Context;
 
 namespace Project_5
 {
@@ -16,6 +13,10 @@ namespace Project_5
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Project5Context>(opt =>
+            {
+                opt.UseSqlServer("server=(localdb)\\mssqllocaldb; database=Project5Core; integrated security=true;");
+            });
             services.AddControllersWithViews();
         }
 
