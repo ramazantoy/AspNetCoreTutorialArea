3e4265570d71483537da24f5215dc6d56ad94f84
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-31 21:42:10 +0300

    Project_6.Common : Response<T> created, Custom Validation errors created

---
+++ Project_6/Project_6.Common/CustomValidationError.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_6.Common
+{
+    public class CustomValidationError
+    {
+        public string Name { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ Project_6/Project_6.Common/IResponse{T}.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Project_6.Common
+{
+    public interface IResponse<T> : IResponse
+    {
+        T Data { get; set; }
+
+        List<CustomValidationError> ValidationErrors { get; set; }
+    }
+}
