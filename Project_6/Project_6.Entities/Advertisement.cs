a2f5efcbd0895bf1436b5b3b5d93e7d93a0f150b
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-31 22:05:48 +0300

    Project_6.Entities : Advertisement created

---
+++ Project_6/Project_6.Entities/Advertisement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_6.Entities
+{
+    public class Advertisement : BaseEntity
+    {
+        public string Title { get; set; }
+        public bool Status { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
