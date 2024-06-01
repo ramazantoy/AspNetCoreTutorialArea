34edccba6e48dfaf55b1160fad13ebb9a29cc27b
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 18:32:18 +0300

    Project_6.DataAcces : AdvertisementContext  created entities properties created

---
+++ Project_6/Project_6.DataAccess/Contexts/AdvertisementContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.Contexts
+{
+    public class AdvertisementContext : DbContext
+    {
+        public AdvertisementContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Advertisement> Advertisements { get; set; }
+
+        public DbSet<AdvertisementAppUser> AdvertisementAppUsers { get; set; }
+
+        public DbSet<AdvertisementUserStatus> AdvertisementUserStatus { get; set; }
+
+        public DbSet<AppRole> AppRoles { get; set; }
+        
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public DbSet<AppUserRole> AppUserRoles { get; set; }
+
+        public DbSet<Gender> Genders { get; set; }
+
+        public DbSet<MilitaryStatus> MilitaryStatus { get; set; }
+        
+        public DbSet<ProvidedService> ProvidedServices { get; set; }
+
+
+
+    }
+}
