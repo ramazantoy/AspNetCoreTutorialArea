db65219e3d035c814987ca0768df15bc7e240305
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 19:36:42 +0300

    Project_6.DataAccess : ProvidedServiceConfiguration created with fluent api

---
+++ Project_6/Project_6.DataAccess/Configurations/ProvidedServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.Configurations
+{
+    public class ProvidedServiceConfiguration : IEntityTypeConfiguration<ProvidedService>
+    {
+        public void Configure(EntityTypeBuilder<ProvidedService> builder)
+        {
+            builder.Property(x => x.Description).HasColumnType("ntext").IsRequired();
+            builder.Property(x => x.ImagePath).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.Title).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.CreatedTime).HasDefaultValueSql("getdate()");
+            
+        }
+    }
+}
