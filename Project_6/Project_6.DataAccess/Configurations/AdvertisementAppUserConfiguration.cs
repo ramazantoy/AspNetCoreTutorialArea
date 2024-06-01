305720e8f12c22fad7fca78a0102f67dbdfe18e0
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 18:48:46 +0300

    Project_6.DataAccess : AdvertisementAppUserConfiguration added with fluent api

---
+++ Project_6/Project_6.DataAccess/Configurations/AdvertisementAppUserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.Configurations
+{
+    public class AdvertisementAppUserConfiguration : IEntityTypeConfiguration<AdvertisementAppUser>
+    {
+        public void Configure(EntityTypeBuilder<AdvertisementAppUser> builder)
+        {
+            builder.HasIndex(x => new
+            {
+                x.AdvertisementId,
+                x.AppUserId
+            }).IsUnique();
+
+            builder.Property(x => x.CvPath).HasMaxLength(500).IsRequired();
+            
+            builder.HasOne(x => x.Advertisement).WithMany(x => x.AdvertisementAppUsers)
+                .HasForeignKey(x => x.AdvertisementId);
+            
+            builder.HasOne(x => x.AppUser).WithMany(x => x.AdvertisementAppUsers)
+                .HasForeignKey(x => x.AppUserId);
+
+            builder.HasOne(x => x.AdvertisementUserStatus).WithMany(x => x.AdvertisementAppUsers)
+                .HasForeignKey(x => x.AdvertisementUserStatusId);
+
+            builder.HasOne(x => x.MilitaryStatus).WithMany(x => x.AdvertisementAppUsers)
+                .HasForeignKey(x => x.MilitaryStatusId);
+        }
+    }
+}
