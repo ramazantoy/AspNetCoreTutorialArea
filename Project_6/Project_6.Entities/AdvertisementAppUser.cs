b60b7030e531d8d1c72b427f7b73da960f9459df
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 17:50:11 +0300

    Project_6.Entities :  AdvertisementAppUser  table created

---
+++ Project_6/Project_6.Entities/AdvertisementAppUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project_6.Entities
+{
+    public class AdvertisementAppUser : BaseEntity
+    {
+        public int AdvertisementId { get; set; }
+
+        public int AppUserId { get; set; }
+
+        public int  AdvertisementUserStatusId { get; set; }
+        
+        public int  MilitaryStatusId { get; set; }
+        
+        public DateTime? EndDate { get; set; }
+
+        public int WorkExperience { get; set; }
+
+        public string CvPath { get; set; }
+        
+ 
+    }
+}
