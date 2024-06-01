3cd94ebacf9b44c4171a752b000c646fe5b11c4f
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 19:06:26 +0300

    Project_6.DataAccces : AppUserRoleConfiguration created with fluent api

---
+++ Project_6/Project_6.DataAccess/Configurations/AppUserRoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.Configurations
+{
+    public class AppUserRoleConfiguration : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            builder.HasIndex(x => new
+            {
+                x.AppRoleId, UserId = x.AppUserId,
+            }).IsUnique();
+
+            builder.HasOne(x => x.AppRole).WithMany(x => x.AppUserRoles).HasForeignKey(x => x.AppRoleId);
+
+            builder.HasOne(x => x.AppUser).WithMany(x => x.AppUserRoles).HasForeignKey(x => x.AppUserId);
+            
+            
+        }
+    }
+}
--- Project_6/Project_6.Entities/AppUserRole.cs
+++ Project_6/Project_6.Entities/AppUserRole.cs
@@ -2,7 +2,7 @@
 {
     public class AppUserRole : BaseEntity
     {
-        public int UserId { get; set; }
+        public int AppUserId { get; set; }
         public AppUser AppUser { get; set; }
         public int AppRoleId { get; set; }
         public AppRole AppRole { get; set; }
