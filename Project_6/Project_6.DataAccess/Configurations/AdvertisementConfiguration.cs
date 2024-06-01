9d36e651cfeb0585cb20f02d1971d857c90eae0b
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 18:37:17 +0300

    Project_6.DataAcces : AdvertisementConfiguration added with fluent api

---
+++ Project_6/Project_6.DataAccess/Configurations/AdvertisementConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.Configurations
+{
+    public class AdvertisementConfiguration : IEntityTypeConfiguration<Advertisement>
+    {
+        public void Configure(EntityTypeBuilder<Advertisement> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Description).HasColumnType("ntext").IsRequired();
+            builder.Property(x => x.CreatedTime).HasDefaultValueSql("getdate()");
+
+        }
+    }
+}
