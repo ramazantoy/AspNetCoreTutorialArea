0b168a53be293eca0e4bd60b7e84ab186342ca30
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 18:04:52 +0300

    Project_6.Entities : ProvidedService table created

---
+++ Project_6/Project_6.Entities/ProvidedService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project_6.Entities
+{
+    public class ProvidedService : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
