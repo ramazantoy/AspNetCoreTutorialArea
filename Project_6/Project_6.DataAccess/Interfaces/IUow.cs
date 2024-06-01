a1ccdcf767770cbed4c5331a1da3a59300e16a23
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-01 20:24:10 +0300

    Project_6.DataAccess : Unit of work created (Uow and IUow)

---
+++ Project_6/Project_6.DataAccess/Interfaces/IUow.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.Interfaces
+{
+    public interface IUow
+    {
+        IRepository<T> GetRepository<T>() where T : BaseEntity;
+        Task SaveChanges();
+    }
+}
---
+++ Project_6/Project_6.DataAccess/UnitOfWork/Uow.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Project_6.DataAccess.Contexts;
+using Project_6.DataAccess.Interfaces;
+using Project_6.DataAccess.Repositories;
+using Project_6.Entities;
+
+namespace Project_6.DataAccess.UnitOfWork
+{
+    public class Uow : IUow
+    {
+        private readonly AdvertisementContext _context;
+
+        public Uow(AdvertisementContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<T> GetRepository<T>() where T : BaseEntity
+        {
+            return new Repository<T>(_context);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
