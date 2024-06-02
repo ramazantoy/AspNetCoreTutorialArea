05b9565e6d99dd08ad8a5311d768b2cdb4949d18
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-02 21:10:54 +0300

    Project_6.Business : Automapper added for provided service

---
+++ Project_6/Project_6.Business/Mappings/AutoMapper/ProvidedServiceProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Project_6.Dtos.ProvidedServiceDtos;
+using Project_6.Entities;
+
+namespace Project_6.Business.Mappings.AutoMapper
+{
+    public class ProvidedServiceProfile : Profile
+    {
+        public ProvidedServiceProfile()
+        {
+            CreateMap<ProvidedServiceCreateDto, ProvidedService>().ReverseMap();
+            CreateMap<ProvidedServiceListDto, ProvidedService>().ReverseMap();
+            CreateMap<ProvidedServiceUpdateDto, ProvidedService>().ReverseMap();
+        }
+    }
+}
