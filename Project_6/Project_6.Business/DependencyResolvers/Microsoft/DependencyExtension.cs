f12b073563b0843c68c4c050eacb44bfaffd477f
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-02 18:50:16 +0300

    UI project created (empty core project) , sql connection added to start up

---
+++ Project_6/Project_6.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project_6.DataAccess.Contexts;
+
+namespace Project_6.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AdvertisementContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Local"));
+            });
+        }
+    }
+}
---
+++ Project_6/Project_6.UI/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Project_6.Business.DependencyResolvers.Microsoft;
+
+namespace Project_6.UI
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; set; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+ 
+     
+        public void ConfigureServices(IServiceCollection services)
+        {
+          services.AddDependencies(Configuration);
+        }
+
+    
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
