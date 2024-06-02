4b6430d2079530272d2427a6ec31417ad7d67751
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-02 21:31:56 +0300

    Project_6.Business : ProvidedServiceManager update

--- Project_6/Project_6.Business/Services/ProvidedServiceManager.cs
+++ Project_6/Project_6.Business/Services/ProvidedServiceManager.cs
@@ -1,23 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using FluentValidation;
 using Project_6.Business.Interfaces;
-using Project_6.Common;
 using Project_6.DataAccess.Interfaces;
-using Project_6.Dtos.Interfaces;
 using Project_6.Dtos.ProvidedServiceDtos;
 using Project_6.Entities;
 
 namespace Project_6.Business.Services
 {
-    public class ProvidedServiceManager : Service<ProvidedServiceCreateDto,ProvidedServiceUpdateDto,ProvidedServiceListDto,Entities.ProvidedService>,IProvidedServiceManager
+    public class ProvidedServiceManager :
+        Service<ProvidedServiceCreateDto, ProvidedServiceUpdateDto, ProvidedServiceListDto, ProvidedService>,
+        IProvidedServiceManager
     {
-        
-        
-        public ProvidedServiceManager(IMapper mapper,IValidator<ProvidedServiceCreateDto> createDtoValidator,IValidator<ProvidedServiceUpdateDto> updateDtoValidator,IUow uow):base(mapper,createDtoValidator,updateDtoValidator,uow)
+        public ProvidedServiceManager(IMapper mapper, IValidator<ProvidedServiceCreateDto> createDtoValidator,
+            IValidator<ProvidedServiceUpdateDto> updateDtoValidator, IUow uow) : base(mapper, createDtoValidator,
+            updateDtoValidator, uow)
         {
-            
         }
     }
 }
