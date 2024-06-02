28eac66141d8ebac6298d27a9cf5e20f8043a519
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-02 20:24:15 +0300

    Project_6.Business : validations added dp injection

--- Project_6/Project_6.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
+++ Project_6/Project_6.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Project_6.Business.ValidationRules.FluentValidation;
 using Project_6.DataAccess.Contexts;
 using Project_6.DataAccess.Interfaces;
 using Project_6.DataAccess.UnitOfWork;
+using Project_6.Dtos.ProvidedServiceDtos;
 
 namespace Project_6.Business.DependencyResolvers.Microsoft
 {
@@ -24,6 +27,9 @@ namespace Project_6.Business.DependencyResolvers.Microsoft
             var mapper = mapperConfiguration.CreateMapper();
             services.AddSingleton(mapper);
             services.AddScoped<IUow, Uow>();
+            
+            services.AddTransient<IValidator<ProvidedServiceCreateDto>, ProvidedServiceCreateDtoValidator>();
+            services.AddTransient<IValidator<ProvidedServiceUpdateDto>, ProvidedServiceUpdateDtoValidator>();
         }
     }
 }
