4bf933a03b025ae7509ef69f62c7bfd18ec9e538
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-02 20:32:05 +0300

    Project_6.Business : Generic Service created <IDto>

---
+++ Project_6/Project_6.Business/Interfaces/IService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Project_6.Common;
+using Project_6.Dtos.Interfaces;
+using Project_6.Dtos.ProvidedServiceDtos;
+
+namespace Project_6.Business.Interfaces
+{
+    public interface IService<TCreateDto, TUpdateDto, TListDto>
+        where TCreateDto : class, IDto, new()
+        where TUpdateDto : class, IDto, new()
+        where TListDto : class, IDto, new()
+
+    {
+        Task<IResponse<TCreateDto>> CreateAsync(TCreateDto createDto);
+
+        Task<IResponse<TUpdateDto>> UpdateAsync(TUpdateDto updateDto);
+
+        Task<IResponse<IDto>> GetByIdAsync(int id);
+
+        Task<IResponse> RemoveAsync(int id);
+
+        Task<IResponse<List<TListDto>>> GetAllAsync();
+    }
+}
