30f7084d94fd85ade39b7a120493a2d8127eaa14
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-04 05:44:10 +0300

    Project_6.UI  : Controller Extensions created

---
+++ Project_6/Project_6.UI/Extensions/ControllerExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_6.Common;
+
+namespace Project_6.UI.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult ResponseRedirectToAction(this Controller controller, IResponse response,
+            string actionName)
+        {
+            if (response.ResponseType == ResponseType.NotFound)
+            {
+                return controller.NotFound();
+            }
+
+            return controller.RedirectToAction(actionName);
+        }
+
+        public static IActionResult ResponseRedirectToAction<T>(this Controller controller, IResponse<T> response,
+            string actionName)
+        {
+            switch (response.ResponseType)
+            {
+                case ResponseType.NotFound:
+                    return controller.NotFound();
+                case ResponseType.ValidationError:
+                {
+                    foreach (var customValidationError in response.ValidationErrors)
+                    {
+                        controller.ModelState.AddModelError(customValidationError.PropertyName,
+                            customValidationError.ErrorMessage);
+                    }
+
+                    return controller.View(response.Data);
+                }
+                case ResponseType.Succes:
+                default:
+                    return controller.RedirectToAction(actionName);
+            }
+        }
+
+
+        public static IActionResult ResponseView<T>(this Controller controller, IResponse<T> response)
+        {
+            if (response.ResponseType == ResponseType.NotFound)
+            {
+                return controller.NotFound();
+            }
+
+            return controller.View(response.Data);
+        }
+    }
+}
