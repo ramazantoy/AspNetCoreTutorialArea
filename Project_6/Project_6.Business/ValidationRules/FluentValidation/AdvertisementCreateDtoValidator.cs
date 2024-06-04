b7034f5ccf71c609167ab2c1cb705b7f1e8af731
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-04 06:50:17 +0300

    Project_6.Business : Advertisement dtos validators created

---
+++ Project_6/Project_6.Business/ValidationRules/FluentValidation/AdvertisementCreateDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Project_6.Dtos.AdvertisementDtos;
+
+namespace Project_6.Business.ValidationRules.FluentValidation
+{
+    public class AdvertisementCreateDtoValidator : AbstractValidator<AdvertisementCreateDto>
+    {
+        public AdvertisementCreateDtoValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+        }
+     
+      
+    }
+}
---
+++ Project_6/Project_6.Business/ValidationRules/FluentValidation/AdvertisementUpdateDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Project_6.Dtos.AdvertisementDtos;
+
+namespace Project_6.Business.ValidationRules.FluentValidation
+{
+    public class AdvertisementUpdateDtoValidator : AbstractValidator<AdvertisementUpdateDto>
+    {
+        public AdvertisementUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            
+        }
+    }
+}
