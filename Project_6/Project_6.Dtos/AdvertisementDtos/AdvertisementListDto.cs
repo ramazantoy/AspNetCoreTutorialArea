f9ae7576f342489e97705fd8c1ec03479caf749d
Ramazan  TOY <ramcotoy06@gmail.com>
2024-06-04 07:32:56 +0300

    Project_6..UI : Opening positions page fixed

--- Project_6/Project_6.Dtos/AdvertisementDtos/AdvertisementListDto.cs
+++ Project_6/Project_6.Dtos/AdvertisementDtos/AdvertisementListDto.cs
@@ -5,6 +5,7 @@ namespace Project_6.Dtos.AdvertisementDtos
 {
     public class AdvertisementListDto : IDto
     {
+        public int  Id { get; set; }
         public string Title { get; set; }
         public bool Status { get; set; }
         public string Description { get; set; }
