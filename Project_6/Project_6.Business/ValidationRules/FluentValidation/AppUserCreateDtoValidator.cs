aa70a180607348e8776f8da7947742ab8650bb86
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-19 16:44:46 +0300

    Project_6.Business : AppUser Validators created with fluent validator

---
+++ Project_6/Project_6.Business/ValidationRules/FluentValidation/AppUserCreateDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Project_6.Dtos.AppUserDtos;
+
+namespace Project_6.Business.ValidationRules.FluentValidation
+{
+    public class AppUserCreateDtoValidator : AbstractValidator<AppUserCreateDto>
+    {
+        public AppUserCreateDtoValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.GenderId).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.UserName).NotEmpty();
+        }
+    }
+}
---
+++ Project_6/Project_6.Business/ValidationRules/FluentValidation/AppUserUpdateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml;
+using FluentValidation;
+using Project_6.Dtos.AppUserDtos;
+
+namespace Project_6.Business.ValidationRules.FluentValidation
+{
+    public class AppUserUpdateDtoValidator : AbstractValidator<AppUserUpdateDto>
+    {
+        public AppUserUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.GenderId).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.UserName).NotEmpty();
+        }
+    }
+}
