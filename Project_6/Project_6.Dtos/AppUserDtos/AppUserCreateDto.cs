38cbf70b72d143c71e00f117fbaf59262f050cf2
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-19 16:38:50 +0300

    Project_6.Dtos : AppUser dtos created

---
+++ Project_6/Project_6.Dtos/AppUserDtos/AppUserCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using Project_6.Dtos.Interfaces;
+
+namespace Project_6.Dtos.AppUserDtos
+{
+    public class AppUserCreateDto : IDto
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public int GenderId { get; set; }
+    }
+}
---
+++ Project_6/Project_6.Dtos/AppUserDtos/AppUserUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using Project_6.Dtos.Interfaces;
+
+namespace Project_6.Dtos.AppUserDtos
+{
+    public class AppUserUpdateDto : IUpdateDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public int GenderId { get; set; }
+    }
+}
