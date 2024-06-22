8255742ada52037825233da521e14cc2b95b780a
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-22 19:03:51 +0300

    Project_6.UI : AccountConroller ceated, sign up added

---
+++ Project_6/Project_6.UI/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Project_6.Business.Interfaces;
+using Project_6.UI.Models;
+
+namespace Project_6.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IGenderService _genderService;
+
+        public AccountController(IGenderService genderService)
+        {
+            _genderService = genderService;
+        }
+
+        // GET
+        public async Task<IActionResult> SignUp()
+        {
+            var response = await _genderService.GetAllAsync();
+            
+            var userCreateModel = new UserCreateModel
+            {
+                Genders = new SelectList(response.Data)
+            };
+            
+            return View(new UserCreateModel());
+        }
+    }
+}
