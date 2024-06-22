333c3812ab8bae57344febeaea9bbf42c18b477c
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-22 18:06:51 +0300

    Project_6.Business : Gender dtos validators created

---
+++ Project_6/Project_6.Business/ValidationRules/FluentValidation/GenderCreateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Project_6.Dtos.GenderDtos;
+
+namespace Project_6.Business.ValidationRules.FluentValidation
+{
+    public class GenderCreateDtoValidator : AbstractValidator<GenderCreateDto>
+    {
+        public GenderCreateDtoValidator()
+        {
+            RuleFor(x => x.Definition).NotNull();
+        }
+    }
+}
---
+++ Project_6/Project_6.Business/ValidationRules/FluentValidation/GenderUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Project_6.Dtos.GenderDtos;
+
+namespace Project_6.Business.ValidationRules.FluentValidation
+{
+    public class GenderUpdateDtoValidator : AbstractValidator<GenderUpdateDto>
+    {
+        public GenderUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Definition).NotEmpty();
+        }
+    }
+}
