2830b37bbd65a8f19d8b71ccf6f3363aa57ac415
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-22 18:59:48 +0300

    Project_6.UI : UserCreateModel values added

--- Project_6/Project_6.UI/Models/UserCreateModel.cs
+++ Project_6/Project_6.UI/Models/UserCreateModel.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Project_6.UI.Models
 {
     public class UserCreateModel
     {
-        //public List<> Type { get; set; }
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public int GenderId { get; set; }
+
+        public SelectList Genders { get; set; }
     }
 }
