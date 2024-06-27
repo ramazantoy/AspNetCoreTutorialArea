177a1d9717761819d137acd88ae752c0e319a88c
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-27 18:35:15 +0300

    Project_6.DataAcces : AppRole added to configuration

--- Project_6/Project_6.DataAccess/Configurations/AppRoleConfiguration.cs
+++ Project_6/Project_6.DataAccess/Configurations/AppRoleConfiguration.cs
@@ -9,6 +9,19 @@ namespace Project_6.DataAccess.Configurations
         public void Configure(EntityTypeBuilder<AppRole> builder)
         {
             builder.Property(x => x.Definition).HasMaxLength(300).IsRequired();
+
+            builder.HasData(new AppRole[]
+            {
+              new ()
+              {
+                  Definition = "Admin",
+                  Id=1,
+              },
+              new (){
+                  Definition = "Member",
+                  Id=2,
+              }
+            });
         }
     }
 }
