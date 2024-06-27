2c9dfbfb79e80b4d2d02740939242342526c3984
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-27 18:39:28 +0300

    Project_6.DataAcces : App role definition migration created database updated

--- Project_6/Project_6.DataAccess/Migrations/20240627153758_AppRoleDataAdded.Designer.cs
+++ Project_6/Project_6.DataAccess/Migrations/20240627153758_AppRoleDataAdded.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project_6.DataAccess.Contexts;
 
 namespace Project_6.DataAccess.Migrations
 {
     [DbContext(typeof(AdvertisementContext))]
-    partial class AdvertisementContextModelSnapshot : ModelSnapshot
+    [Migration("20240627153758_AppRoleDataAdded")]
+    partial class AppRoleDataAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
