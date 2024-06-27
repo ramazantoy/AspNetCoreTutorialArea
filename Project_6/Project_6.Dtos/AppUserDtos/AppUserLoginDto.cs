c5ea6697b9902b1754a9054d86709d3e1158c9da
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-27 20:04:51 +0300

    Project_6.Dtos : AppUserLoginDto created

---
+++ Project_6/Project_6.Dtos/AppUserDtos/AppUserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_6.Dtos.AppUserDtos
+{
+    public class AppUserLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
