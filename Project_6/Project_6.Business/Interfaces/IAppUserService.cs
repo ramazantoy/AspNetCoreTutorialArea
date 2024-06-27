2d6d9c738015eed7fba92b6e068854cff7c8578d
Ramazan Toy <ramcotoy06@gmail.com>
2024-06-27 18:58:33 +0300

    Project_6.Business : CreateWithRoleAsync added to appuserservice

--- Project_6/Project_6.Business/Interfaces/IAppUserService.cs
+++ Project_6/Project_6.Business/Interfaces/IAppUserService.cs
@@ -1,10 +1,12 @@
-﻿using Project_6.Dtos.AppUserDtos;
+﻿using System.Threading.Tasks;
+using Project_6.Common;
+using Project_6.Dtos.AppUserDtos;
 using Project_6.Entities;
 
 namespace Project_6.Business.Interfaces
 {
     public interface IAppUserService : IService<AppUserCreateDto,AppUserUpdateDto,AppUserListDto,AppUser>
     {
-        
+        Task<IResponse<AppUserCreateDto>> CreateWithRoleAsync(AppUserCreateDto createDto,int roleId)
     }
 }
--- Project_6/Project_6.Business/Services/AppUserService.cs
+++ Project_6/Project_6.Business/Services/AppUserService.cs
@@ -1,19 +1,52 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
 using FluentValidation;
+using Project_6.Business.Extensions;
 using Project_6.Business.Interfaces;
+using Project_6.Common;
 using Project_6.DataAccess.Interfaces;
 using Project_6.Dtos.AppUserDtos;
-
 using Project_6.Entities;
 
 namespace Project_6.Business.Services
 {
-    public class AppUserService : Service<AppUserCreateDto,AppUserUpdateDto,AppUserListDto,AppUser>,IAppUserService
+    public class AppUserService : Service<AppUserCreateDto, AppUserUpdateDto, AppUserListDto, AppUser>, IAppUserService
     {
-        public AppUserService(IMapper mapper,IValidator<AppUserCreateDto> createDtoValidator,IValidator<AppUserUpdateDto> updateDtoValidator,IUow uow)
-            : base(mapper,createDtoValidator,updateDtoValidator,uow)
+        private readonly IMapper _mapper;
+        private readonly IUow _uow;
+        private readonly IValidator<AppUserCreateDto> _createValidator;
+
+        public AppUserService(IMapper mapper, IValidator<AppUserCreateDto> createDtoValidator,
+            IValidator<AppUserUpdateDto> updateDtoValidator, IUow uow, IValidator<AppUserCreateDto> createValidator)
+            : base(mapper, createDtoValidator, updateDtoValidator, uow)
         {
+            _mapper = mapper;
+            _uow = uow;
+            _createValidator = createValidator;
+        }
+
+        public async Task<IResponse<AppUserCreateDto>> CreateWithRoleAsync(AppUserCreateDto createDto,int roleId)
+        {
+            var validationResult = await _createValidator.ValidateAsync(createDto);
             
+            if (!validationResult.IsValid) return new Response<AppUserCreateDto>(createDto, validationResult.ConvertToCustomValidationError());
+           
+            
+            var mappedUser = _mapper.Map<AppUser>(createDto);
+            mappedUser.AppUserRoles = new List<AppUserRole>
+            {
+                new()
+                {
+                    AppUser = mappedUser,
+                    AppRoleId = roleId,
+                }
+            };
+            await _uow.GetRepository<AppUser>().CreateAsync(mappedUser);
+
+            await _uow.SaveChanges();
+            return new Response<AppUserCreateDto>(ResponseType.Succes, createDto);
+
         }
     }
 }
