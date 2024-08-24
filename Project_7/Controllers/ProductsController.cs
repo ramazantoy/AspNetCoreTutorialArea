e599606f9b1cda7c186a1d1531b8bec2e0c23f15
Ramazan  TOY <ramcotoy06@gmail.com>
2024-08-24 16:14:42 +0300

    dummy ProductsController api created

---
+++ Project_7/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_7.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[] { new { Name = "Computer", Price = 3500 }, new { Name = "Phone", Price = 1000 } });
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(new[] { new { Name = "Computer", Price = 3500 } } );
+        }
+    }
+}
