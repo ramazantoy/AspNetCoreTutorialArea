aca3d17d63c67b7bb2f9f7563ce74d28070ac546
Ramazan  TOY <ramcotoy06@gmail.com>
2024-08-24 16:22:48 +0300

    EntityFrame work packages added to project

---
+++ Project_7/Data/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_7.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int  Stock { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
