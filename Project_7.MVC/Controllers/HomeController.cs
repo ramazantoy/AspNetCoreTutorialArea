4c2e6426f6d608de7cfed7396ce7505f67a5df95
Ramazan  TOY <ramcotoy06@gmail.com>
2024-08-26 16:54:35 +0300

    Project_7.MVC : join to api/products added

--- Project_7.MVC/Controllers/HomeController.cs
+++ Project_7.MVC/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project_7.MVC.ResponseModels;
 
 namespace Project_7.MVC.Controllers
 {
     public class HomeController : Controller
     {
-     
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5000/api/Products");
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK) return View(null);
+            
+            
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<ProductResponseModel>>(jsonData);
+            return View(result);
+
         }
     }
 }
---
+++ Project_7.MVC/ResponseModels/ProductResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project_7.MVC.ResponseModels
+{
+    public class ProductResponseModel
+    {
+        public int Id { get; set; }
+        public int  Stock { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ImagePath { get; set; }
+
+        public int? CategoryId { get; set; }
+        
+    }
+}
