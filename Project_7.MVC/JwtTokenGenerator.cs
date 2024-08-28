009a11013121682195dc697257cae7904d135061
Ramazan  TOY <ramcotoy06@gmail.com>
2024-08-28 16:23:17 +0300

    Project_7.MVC : Generate Token added

---
+++ Project_7.MVC/JwtTokenGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Project_7.MVC
+{
+    public class JwtTokenGenerator
+    {
+        public string GenerateToken()
+        {
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("afoolasfallen258"));
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost",
+                notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(token);
+        }
+    }
+}
