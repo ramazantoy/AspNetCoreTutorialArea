1c79f23dd003bb5b56f810e9030383b7926e1cd7
Ramazan  TOY <ramcotoy06@gmail.com>
2024-08-30 17:02:56 +0300

    Project_8.CQRS entity packages installed, Student Context created

---
+++ Project_8.CQRS/Data/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_8.CQRS.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+            
+        }
+    }
+}
