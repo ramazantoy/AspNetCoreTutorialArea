d0a4bf4615dbfde1d84f3ae448f86c9f79586562
Ramazan  TOY <ramcotoy06@gmail.com>
2024-08-31 20:45:42 +0300

    Project_.9.Back : Generic Repository created

---
+++ Project_9.JWT.NET6/Project_9.Back/Core/Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Project_9.Back.Core.Application.Interfaces;
+
+public interface IRepository<T> where T : class,new()
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(object id);
+    Task<T?> GetByFilter(Expression<Func<T, bool>> filter);
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task RemoveAsync(T entity);
+
+}
---
+++ Project_9.JWT.NET6/Project_9.Back/Persistance/Repositories/Repository{T}.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Project_9.Back.Core.Application.Interfaces;
+using Project_9.Back.Persistance.Context;
+
+namespace Project_9.Back.Persistance.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class, new()
+{
+    private readonly Project9JwtContext _context;
+
+    public Repository(Project9JwtContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().AsNoTracking().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(object id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T?> GetByFilter(Expression<Func<T, bool>> filter)
+    {
+        return await _context.Set<T>().AsNoTracking().SingleOrDefaultAsync(filter);
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public  async Task UpdateAsync(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RemoveAsync(T entity)
+    { 
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
