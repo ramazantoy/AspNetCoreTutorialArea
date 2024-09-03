db2b54b317c401e4ffb7785041e9bf86e0f3aa2d
Ramazan  TOY <ramcotoy06@gmail.com>
2024-09-03 18:22:04 +0300

    Project_9.Front : Dummy HomeController created view added

--- Project_9.JWT.NET6/Project_9.Front/Program.cs
+++ Project_9.JWT.NET6/Project_9.Front/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
