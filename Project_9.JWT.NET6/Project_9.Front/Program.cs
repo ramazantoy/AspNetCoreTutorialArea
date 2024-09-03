88c5c3bffe1578f73ec332626af5692e2248d0e8
Ramazan  TOY <ramcotoy06@gmail.com>
2024-09-03 18:52:25 +0300

    Project_9.Front : Cookie Configuration created

--- Project_9.JWT.NET6/Project_9.Front/Program.cs
+++ Project_9.JWT.NET6/Project_9.Front/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme,
+    opt =>
+    {
+        opt.LoginPath = "/Account/Login";
+        opt.LogoutPath = "/Account/Logout";
+        opt.AccessDeniedPath = "/Account/AccessDenied";
+        opt.Cookie.SameSite = SameSiteMode.Strict;
+        opt.Cookie.HttpOnly = true;
+        opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+        opt.Cookie.Name = "Project_9.Cookie";
+    });
 var app = builder.Build();
 
 
