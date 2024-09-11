57ca5be0bbd16375733ae5debf6008d46402c940
Ramazan  TOY <ramcotoy06@gmail.com>
2024-09-11 16:48:43 +0300

    Project_10.RabbitMQ : remove msg added for consumer

--- Project_10.RabbitMQ/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
+++ Project_10.RabbitMQ/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -9,7 +9,8 @@ consumer.Received += (model, ea) =>
 {
     var byteMessages = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(byteMessages);
+    channel.BasicAck(ea.DeliveryTag,multiple:false);
     Console.WriteLine($"Message : {message}");
 };
-channel.BasicConsume(queue:"queueTest",autoAck:true,consumer:consumer);
+channel.BasicConsume(queue:"queueTest",autoAck:false,consumer:consumer);
 Console.ReadKey();
