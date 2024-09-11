801c3c3bff7aa0675c8acb3145f22d0fc731103d
Ramazan  TOY <ramcotoy06@gmail.com>
2024-09-11 16:34:49 +0300

    Project_10.RabbitMQ  : Dummy  Producer and Consumer added

--- Project_10.RabbitMQ/Project_10.RabbitMQ.Producer/Program.cs
+++ Project_10.RabbitMQ/Project_10.RabbitMQ.Producer/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text;
+using RabbitMQ.Client;
+
+var factory=new ConnectionFactory()
+{
+    HostName = "localhost",
+   // Uri = new Uri("")
+};
+var connection=factory.CreateConnection();
+var channel=connection.CreateModel();
+
+channel.QueueDeclare(queue:"queueTest",durable:false,exclusive:false,autoDelete: false,arguments:null);
+
+var message="Hello, RabbitMQ";
+var byteMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange:"",routingKey:"queueTest",basicProperties:null,body:byteMessage);
+Console.WriteLine(" [x] Sent {0}", message);
+Console.ReadKey();
+
---
+++ Project_10.RabbitMQ/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+var channel = factory.CreateConnection().CreateModel();
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var byteMessages = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(byteMessages);
+    Console.WriteLine($"Message : {message}");
+};
+channel.BasicConsume(queue:"queueTest",autoAck:true,consumer:consumer);
+Console.ReadKey();
